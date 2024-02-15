112064862217006afa0668eea05a238d7ca0cb1d
VNK6307 <vnkushchenko@gmail.com>
2024-02-15 17:06:25 +0300

    Code fixed

--- select_alexey.sql
+++ select_alexey.sql
@@ -1,4 +1,4 @@
 SELECT product_name
-FROM ORDERS
-JOIN CUSTOMERS ON ORDERS.customer_id = CUSTOMERS.customers_id
-WHERE name COLLATE Latin1_General_CI_AS = 'alexey';
+FROM   ORDERS
+JOIN   CUSTOMERS ON ORDERS.customer_id = CUSTOMERS.customers_id
+WHERE  name COLLATE Latin1_General_CI_AS = 'alexey';
